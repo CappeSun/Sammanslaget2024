ccd01d24d85a39ad3f90f48e2b2dbc6078f0e6c2
William Davidson <wj.davidson@outlook.com>
2024-08-23 09:05:31 +0200

    added game styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,19 +3,30 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      },
       colors: {
         'light-purple': '#BFBDF9',
         'medium-purple': '#9D9BEC',
-        'dark-purple': '#060872'
+        'dark-purple': '#060872',
+        'bingo-green': '#00CA64',
+        'bingo-dark-green': 'rgba(0, 202, 100, 0.30)'
       },
       keyframes: {
         'caret-blink': {
           '0%,70%,100%': { opacity: '1' },
           '20%,50%': { opacity: '0' }
+        },
+        'spin-slow': {
+          to: {
+            transform: 'rotate(360deg)'
+          }
         }
       },
       animation: {
-        'caret-blink': 'caret-blink 1.25s ease-out infinite'
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
+        'spin-slow': 'spin-slow 2s linear infinite'
       },
       boxShadow: {
         'button-shadow': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'
